db2bbb718bc6ac678ecad55b07fab2acaef44a5d
Rimuru <Rimuru.Dev@gmail.com>
2022-12-01 00:28:08 +0400

    Update ClickHandler.cs

--- Simple-Puzzle/Assets/InternalAssets/Scripts/UI/ClickHandler.cs
+++ Simple-Puzzle/Assets/InternalAssets/Scripts/UI/ClickHandler.cs
@@ -9,17 +9,27 @@ namespace RimuruDev
 
         private void Start()
         {
-            EventTrigger eventTrigger = GetComponent<EventTrigger>();
+            EventTrigger eventTrigger;
+
+            if (TryGetComponent<EventTrigger>(out var trigger))
+                eventTrigger = trigger;
+            else
+                eventTrigger = gameObject.AddComponent<EventTrigger>().GetComponent<EventTrigger>();
+
             EventTrigger.Entry eventClick = new EventTrigger.Entry();
 
             eventClick.eventID = EventTriggerType.PointerClick;
-            eventTrigger.triggers.Add(eventClick);
 
-            eventClick.callback.AddListener((eventData) =>
+            if (eventClick != null)
             {
-                if (audioSource != null)
-                    audioSource.Play();
-            });
+                eventTrigger.triggers.Add(eventClick);
+
+                eventClick.callback.AddListener((eventData) =>
+                {
+                    if (audioSource != null)
+                        audioSource.Play();
+                });
+            }
         }
     }
 }
